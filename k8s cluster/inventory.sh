19ab4a43eb28352d703d025293384191b07c8adf
chiheb-devops <166850971+chiheb-devops@users.noreply.github.com>
2024-06-19 13:05:40 +0100

    Update inventory.sh

--- k8s cluster/inventory.sh
+++ k8s cluster/inventory.sh
@@ -2,4 +2,4 @@
 
 export master=$(cat /home/ubuntu/master)
 export worker=$(cat /home/ubuntu/worker)
-echo -e  "[master_node] \n master ansible_host=$master ansible_ssh_private_key_file=pfe-key.pem \n[worker_node] \n worker ansible_host=$worker ansible_ssh_private_key_file=pfe-key.pem \n\n" > inventory
+echo -e  "[master_node] \n master ansible_host=$master ansible_ssh_private_key_file=/home/ubuntu/pfe-key.pem \n[worker_node] \n worker ansible_host=$worker ansible_ssh_private_key_file=/home/ubuntu/pfe-key.pem \n\n" > inventory
